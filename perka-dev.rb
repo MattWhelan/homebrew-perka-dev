8074ed88ece8cb59f9717234ec3b144420d06fd9
Matt Whelan <matt@blacklogik.com>
2014-10-24 12:28:54 -0700

    initial commit

---
+++ perka-dev.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class PerkaDev < Formula
+  homepage 'https://perka.github.com/perka'
+
+  depends_on 'pcre'
+  depends_on 'v8'
+  depends_on 'node'
+
+  def install
+    system "sudo", "gem", "uninstall", "rkelly"
+    system "sudo", "gem", "install", "rkelly-remix"
+    system "sudo", "gem", "install", "closure-compiler", "yui-compressor"
+    system "sudo", "gem", "install", "-v", "3.3.9", "sass"
+    system "sudo", "gem", "install", "-v", "0.25.1", "scss-lint"
+    system "sudo", "gem", "install", "hpricot", "premailer"
+
+    system "sudo", "npm", "install", "-g", "gulp"
+    system "sudo", "npm", "install", "-g", "grunt-cli"
+  end
+end
